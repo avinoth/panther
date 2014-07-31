df0a2160cb57abb42e563a78d5ed7888f6469238
Vinoth kumar A <bluerail@outlook.com>
2014-07-31 20:22:31 +0530

    refurbishment

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = True
-CSRF_ENABLED = True
-SECRET_KEY = 'rc#41he@q$o&w^019_h1%)-41-6^omcb5x3c14cd22dcz8r*tl'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
