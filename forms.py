0a7718877d5a89a22af8ac8f4459fff37a6bc0d8
Vinoth kumar A <bluerail@outlook.com>
2014-08-01 23:08:24 +0530

    pyc go go

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField
-from wtforms.validators import Required
-
-class PostForm(Form):
-    title = TextField('title', validators = [Required()])
-    body = TextAreaField('body', validators = [Required()])
-    
